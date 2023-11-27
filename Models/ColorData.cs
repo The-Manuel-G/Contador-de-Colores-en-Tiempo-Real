7aff4fe2fbb3ca97ed147eb8404015776b2cc418
The-Manuel-G <arielnaranjoi829@gmail.com>
2023-11-26 21:31:07 -0400

    Proyecto BASE

---
+++ Models/ColorData.cs
@@ -0,0 +1,10 @@
+// En el archivo ColorData.cs en la carpeta "Models" del proyecto Blazor WebAssembly
+namespace colorin.Models
+{
+    public class ColorData
+    {
+        public int RedCount { get; set; }
+        public int GreenCount { get; set; }
+        public int BlueCount { get; set; }
+    }
+}
---
+++ Models/ColorModel.cs
@@ -0,0 +1,12 @@
+// ColorModel.cs
+
+namespace colorin.Models
+{
+
+public class ColorModel
+{
+    public int RedCount { get; set; }
+    public int GreenCount { get; set; }
+    public int BlueCount { get; set; }
+}
+}
