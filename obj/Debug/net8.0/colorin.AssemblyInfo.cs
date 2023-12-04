3a8d7bd8444799542090fd5fb64b96cf15158de5
The-Manuel-G <arielnaranjoi829@gmail.com>
2023-12-04 14:54:10 -0400

    creacion de carpeta de imagenes y colocacion de imagen

--- obj/Debug/net8.0/colorin.AssemblyInfo.cs
+++ obj/Debug/net8.0/colorin.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("colorin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ffeee65a3e67e5c27964ac495cdb8ec5b7b91b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce73246bc7dd6d3d7749ee47b5e3f7750e83261")]
 [assembly: System.Reflection.AssemblyProductAttribute("colorin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("colorin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
