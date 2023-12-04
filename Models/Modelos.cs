195c034b2cf10cb4119be6cc3d9e7328c0b4f149
The-Manuel-G <arielnaranjoi829@gmail.com>
2023-12-04 09:39:09 -0400

    mantenimiento a la rama develop

--- Models/Modelos.cs
+++ Models/Modelos.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace tarea4.Data;
+namespace colorin.Models;
+
+
 
 
 public class Persona {  
@@ -62,3 +64,4 @@ public enum PasoDelProceso{
 
 }
 
+}
